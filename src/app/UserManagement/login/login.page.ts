b497cee0aa1aa7db9af8ba1fbfe14f553978cd92
Paul Dederer <paul.dederer@icloud.com>
2019-06-06 16:30:06 +0200

    Login with provider changed to signInWithRedirect

--- src/app/UserManagement/login/login.page.ts
+++ src/app/UserManagement/login/login.page.ts
@@ -64,7 +64,7 @@ export class LoginPage implements OnInit {
   }
 
   signInProvider(provider){
-    this.fire.auth().signInWithPopup(provider).then(function(result) {
+    this.fire.signInWithRedirect(provider).then(function(result) {
       var token = result.credential.accessToken;
       var user = result.user;
     }).catch(function(error) {
